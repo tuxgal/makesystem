8d9dc0b0dd09a424c398f4980f9269e9d3f029ee
G3 <tuxgal@Baymax.area51>
2022-02-21 20:39:11 -0800

    feat: Adding color support.

---
+++ colors.mk
@@ -0,0 +1,17 @@
+ifndef COLORS_MK
+COLORS_MK := 1
+
+# Disable the colorized output from make if either
+# explicitly overridden or if no tty is attached.
+DISABLE_COLORS ?= $(shell [ -t 0 ] && echo no)
+
+# Configure colors.
+ifeq ($(DISABLE_COLORS),no)
+    COLOR_BLUE    := \x1b[1;34m
+    COLOR_RESET   := \x1b[0m
+else
+    COLOR_BLUE    :=
+    COLOR_RESET   :=
+endif
+
+endif
