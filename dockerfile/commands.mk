0239468a1d10dc7154c821a8d2b09373a6fbf9b6
G3 <tuxgal@Baymax.area51>
2022-02-23 21:13:12 -0800

    feat: Enable support for docker buildkit.

--- dockerfile/commands.mk
+++ dockerfile/commands.mk
@@ -3,10 +3,19 @@ DOCKERFILE_COMMANDS_MK := 1
 
 include $(MAKESYSTEM_BASE_DIR)/dockerfile/toolchain.mk
 
+BUILD_OPTIONS :=
+
+ifeq ($(ENABLE_DOCKER_BUILDKIT),y)
+    BUILD_OPTIONS += DOCKER_BUILDKIT=1
+    ifeq ($(DOCKER_BUILDKIT_PROGRESS_PLAIN),y)
+        BUILD_OPTIONS += BUILDKIT_PROGRESS=plain
+    endif
+endif
+
 # Commands invoked from rules.
 DUMP_BUILD_ARGS         := ./scripts/build-args.sh
 UPDATE_PACKAGES_INSTALL := ./scripts/update-packages-install.sh
-DOCKERBUILD             := $(DOCKER_CMD) build $(shell $(DUMP_BUILD_ARGS) docker-flags)
+DOCKERBUILD             := $(BUILD_OPTIONS) $(DOCKER_CMD) build $(shell $(DUMP_BUILD_ARGS) docker-flags)
 DOCKERTEST              := IMAGE=$(FULL_IMAGE_NAME) ./scripts/test.sh
 DOCKERLINT              := $(DOCKER_CMD) run --rm -i hadolint/hadolint:v2.8.0 hadolint - <
 
