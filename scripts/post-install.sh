e05216fde272dd55aec1498fbfc11e97ba83c67b
G3 <tuxgal@Baymax.area51>
2022-02-26 02:36:18 -0800

    feat: Add Upgrade Makesystem bot workflow.

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -3,6 +3,7 @@
 install_type="$(cat .bootstrap/TYPE)"
 
 rm -rf .github/ && cp -rf .makesystem/${install_type:?}/github .github
+cp .makesystem/common/github/workflows/* .github/workflows/
 
 rm -rf .bootstrap/ && cp -rf .makesystem/bootstrap .bootstrap
 echo "${install_type:?}" > .bootstrap/TYPE
