766443eb68e80ef7788edc87a29a948a695cf7b1
G3 <tuxgal@Baymax.area51>
2022-03-01 15:50:12 -0800

    feat: Use buildkit as the sole means for building docker images.

--- dockerfile/commands.mk
+++ dockerfile/commands.mk
@@ -5,17 +5,14 @@ include $(MAKESYSTEM_BASE_DIR)/dockerfile/toolchain.mk
 
 BUILD_OPTIONS :=
 
-ifeq ($(ENABLE_DOCKER_BUILDKIT),y)
-    BUILD_OPTIONS += DOCKER_BUILDKIT=1
-    ifeq ($(DOCKER_BUILDKIT_PROGRESS_PLAIN),y)
-        BUILD_OPTIONS += BUILDKIT_PROGRESS=plain
-    endif
+ifeq ($(DOCKER_BUILD_PROGRESS_PLAIN),y)
+    BUILD_OPTIONS += --progress=plain
 endif
 
 # Commands invoked from rules.
 DUMP_BUILD_ARGS         := ./scripts/build-args.sh
 UPDATE_PACKAGES_INSTALL := ./scripts/update-packages-install.sh
-DOCKERBUILD             := $(BUILD_OPTIONS) $(DOCKER_CMD) build $(shell $(DUMP_BUILD_ARGS) docker-flags)
+DOCKERBUILD             := $(DOCKER_CMD) buildx build $(BUILD_OPTIONS) $(shell $(DUMP_BUILD_ARGS) docker-flags)
 DOCKERTEST              := IMAGE=$(FULL_IMAGE_NAME) ./scripts/test.sh
 DOCKERLINT              := $(DOCKER_CMD) run --rm -i hadolint/hadolint:v2.8.0 hadolint - <
 
