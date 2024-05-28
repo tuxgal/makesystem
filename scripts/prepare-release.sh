563f0bb74d87babe470c9913e52461525fb4cbb7
G3 <tuxgal@Baymax.area51>
2024-05-28 12:14:21 -0700

    feat: Add a script for preparing releases.

---
+++ scripts/prepare-release.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+script_parent_dir="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+git_repo_dir="$(realpath "${script_parent_dir:?}/..")"
+
+get_latest_tag() {
+    git tag --list | sort --version-sort --reverse | head -1
+}
+
+get_next_semantic_ver() {
+    echo "${1:?}" | awk -F. -v OFS=. '{$NF += 1 ; print}'
+}
+
+if [ -z "$1" ]; then
+    # Generate the next semantic version number if version number is not supplied.
+    rel_ver="$(get_next_semantic_ver $(get_latest_tag))"
+else
+    # Use the supplied version number from the command line arg.
+    rel_ver="${1:?}"
+fi
+
+echo "Creating tag ${rel_ver:?}"
+git githubtag -m "${rel_ver:?} release." ${rel_ver:?}
