3d5efd9fd402fa05f61440abcf2e7ccc7a5654da
G3 <tuxgal@Baymax.area51>
2024-06-05 13:56:31 -0700

    feat: Add generic support for prepare-release.

--- dockerfile/commands.mk
+++ dockerfile/commands.mk
@@ -23,7 +23,7 @@ endif
 DUMP_BUILD_ARGS         := ./.makesystem/dockerfile/scripts/build-args.sh
 UPDATE_PACKAGES_INSTALL := ./.makesystem/dockerfile/scripts/update-packages-install.sh
 UPDATE_LATEST_UPSTREAM  := ./.makesystem/dockerfile/scripts/update-latest-upstream.sh
-PREPARE_RELEASE         := ./scripts/prepare-release.sh
+PREPARE_RELEASE         := ./.makesystem/dockerfile/scripts/prepare-release.sh
 DOCKERBUILD             := $(DOCKER_CMD) buildx build $(BUILD_OPTIONS) $(shell $(DUMP_BUILD_ARGS) docker-flags)
 DOCKERTEST              := IMAGE=$(FULL_IMAGE_NAME) ./.makesystem/dockerfile/scripts/test.sh
 DOCKERLINT              := $(DOCKER_CMD) run --rm -i hadolint/hadolint:v2.8.0 hadolint - <
---
+++ dockerfile/scripts/prepare-release.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e -o pipefail
+
+script_dir="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+repo_dir="$(realpath "${script_dir:?}/../../..")"
+
+source ${repo_dir:?}/.makesystem/dockerfile/scripts/common
+source ${repo_dir:?}/metadata/metadata
+
+if [ -z "$1" ]; then
+    prepare_release
+else
+    prepare_release "${1:?}"
+fi
