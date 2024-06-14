94c6dc45567f85b0d65e2d830e4ceb5e6e928250
G3 <tuxgal@Baymax.area51>
2024-06-14 16:07:05 -0700

    feat: Use ls-remote to identify the latest makesystem tag/version instead.

--- common/upgrade.mk
+++ common/upgrade.mk
@@ -1,7 +1,7 @@
 ifndef COMMON_UPGRADE_MK
 COMMON_UPGRADE_MK := 1
 
-LATEST_MAKESYSTEM_VERSION := curl --silent https://api.github.com/repos/tuxdude/makesystem/tags?per_page=1 | jq --raw-output '.[0].name' | sed -E 's;^v(.*)$$;\1;'
+LATEST_MAKESYSTEM_VERSION := git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags https://github.com/tuxdude/makesystem/ '*.*.*' | cut -d '/' -f 3 | sort --version-sort --reverse | head -1 | sed -E 's@^v(.*)$$@\1@g'
 
 makesystem_upgrade:
 	@./.bootstrap/setup-makesystem.sh $$($(LATEST_MAKESYSTEM_VERSION)) "$(MAKESYSTEM_BASE_DIR)" --upgrade
